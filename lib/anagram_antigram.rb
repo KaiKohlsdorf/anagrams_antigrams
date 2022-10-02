e692626723d1a6551c173e48d79d7d99f553808a
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-02 02:00:23 -0700

    Fifth test passing.

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -5,16 +5,16 @@ class Anagrams
   end
   
   def anagram() 
-    word_one = @word_one.gsub(/\s+/, "")
-    word_two = @word_two.gsub(/\s+/, "")
+    word_one = @word_one.downcase.delete(' ').gsub(/[^0-9A-Za-z]/, "")
+    word_two = @word_two.downcase.delete(' ').gsub(/[^0-9A-Za-z]/, "")
   
-    array_one = word_one.downcase.split("")
-    array_two = word_two.downcase.split("")
+    array_one = word_one.split("")
+    array_two = word_two.split("")
 
     if !(array_one & array_two).any?()
-    "These words have no letter matches and are antigrams."
+    "These words or phrases have no letter matches and are antigrams."
     elsif array_one.all? { |array_one| array_two.include?(array_one) } == true && array_one.length == array_two.length
-      'These words are anagrams!'
+      'These words or phrases are anagrams!'
     elsif word_one =~ /["a", "e", "i", "o", "u", "y"]/ || array_two =~ /["a", "e", "i", "o", "u", "y"]/
       'You need to input actual words!'
     end
--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -4,12 +4,12 @@ require('anagram_antigram')
 describe("#anagram") do
   it("checks if two words are anagrams") do
     cat = Anagrams.new("cat", "act") 
-    expect(cat.anagram()).to(eq("These words are anagrams!"))
+    expect(cat.anagram()).to(eq("These words or phrases are anagrams!"))
   end
 
   it("accounts for the possibility that words might have different cases but should still be anagrams") do
     dog = Anagrams.new("Dog", "God")
-    expect(dog.anagram()).to(eq("These words are anagrams!"))
+    expect(dog.anagram()).to(eq("These words or phrases are anagrams!"))
   end
 
   it("checks that the input has at least one vowel") do
@@ -19,10 +19,11 @@ describe("#anagram") do
 
   it("checks if the inputs are antigrams") do
     antigram = Anagrams.new("antigram", "spy")
-    expect(antigram.anagram()).to(eq("These words have no letter matches and are antigrams."))
+    expect(antigram.anagram()).to(eq("These words or phrases have no letter matches and are antigrams."))
   end
 
-  # it("accounts for two sentences being compared as anagrams or 'antigrams'") do
-
-
+  it("accounts for two sentences being compared as anagrams or 'antigrams'") do
+    sentence = Anagrams.new("Elvis Presley.", "Presley lives!")
+    expect(sentence.anagram()).to(eq("These words or phrases are anagrams!"))
+  end
 end
