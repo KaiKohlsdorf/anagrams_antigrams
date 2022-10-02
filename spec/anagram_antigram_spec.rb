8bf4b5b40b131c29244dab20dc8549c3d8377e92
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-02 01:07:59 -0700

    Second test passing.

--- spec/anagram_antigram_spec.rb
+++ spec/anagram_antigram_spec.rb
@@ -1,11 +1,22 @@
 require('rspec')
 require('anagram_antigram')
 
-describe(Anagrams) do
-  describe("#anagram") do
-    it("checks if two words are anagrams") do
-      cat = Anagrams.new("cat", "act") 
-      expect(cat.anagram()).to(eq("These words are anagrams!"))
-    end
+describe("#anagram") do
+  it("checks if two words are anagrams") do
+    cat = Anagrams.new("cat", "act") 
+    expect(cat.anagram()).to(eq("These words are anagrams!"))
   end
+
+  it("accounts for the possibility that words might have different cases but should still be anagrams") do
+    dog = Anagrams.new("Dog", "God")
+    expect(dog.anagram()).to(eq("These words are anagrams!"))
+  end
+  
+    # it("checks if the inputs are words") do
+
+  # it("checks if the inputs are antigrams") do
+
+  # it("accounts for two sentences being compared as anagrams or 'antigrams'") do
+
+
 end
