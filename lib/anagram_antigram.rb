d652de24f0c462e5a498a7d3710677b022cb78be
Kai Kohlsdorf <kai.w.kohlsdorf@gmail.com>
2022-10-10 18:06:31 -0700

    Removed commented out code.

--- lib/anagram_antigram.rb
+++ lib/anagram_antigram.rb
@@ -7,18 +7,6 @@ class Anagrams
   def anagram() 
     word_one = @word_one.downcase.delete(' ').gsub(/[^A-Za-z]/, "")
     word_two = @word_two.downcase.delete(' ').gsub(/[^A-Za-z]/, "")
-  
-    # array_one = word_one.split("")
-    # array_two = word_two.split("")
-    # vowels_array = "aeiouy".split("")
-    
-    # if !(array_one & array_two).any?()
-    #   "These words or phrases have no letter matches and are antigrams."
-    # elsif array_one.all? { |letter| array_two.include?(letter) } == true && array_one.length == array_two.length
-    #   'These words or phrases are anagrams!'
-    # elsif word_one =~ /["a", "e", "i", "o", "u", "y"]/ || array_two =~ /["a", "e", "i", "o", "u", "y"]/
-    #   'You need to input actual words!'
-    # end
 
     if word_one !~ /[aeiouy]/ || word_two !~ /[aeiouy]/
       'You need to input actual words!'
